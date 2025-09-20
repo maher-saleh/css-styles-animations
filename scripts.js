de776f911a4cb0da790a44031290a3db564f7f3b
Maher Saleh <ehabelnahas@hotmail.com>
2025-09-21 01:02:52 +0300

    Init commit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,18 @@ window.addEventListener('load', () => {
     loadComponent('./buttons/buttons.html', 'container');
 });
 
+// mouse move
+document.addEventListener('mousemove', e => {
+    gsap.to('.text', {
+        x: e.clientX,
+        y: e.clientY,
+        stagger: 0.05
+    })
+})
+
+
 function loadComponent(url, containerId) {
+    // buttons
     fetch('./buttons/buttons.html')
         .then(res => res.text())
         .then(html => {
@@ -11,6 +22,7 @@ function loadComponent(url, containerId) {
             template.innerHTML = html;
             document.getElementById(containerId).appendChild(template.content);
         })
+
         // pixel buttons
         .then(pixel____buttons => {
             let pixelButtons = document.querySelectorAll('.pixel-btn');
@@ -34,6 +46,31 @@ function loadComponent(url, containerId) {
                     }
                 }
             });
+        })
+        .catch(err => console.error(err));
+
+    // cursor
+    fetch('./cursor/cursor.html')
+        .then(res => res.text())
+        .then(html => {
+            const template = document.createElement('template');
+            template.id = 'cursor-container';
+            template.innerHTML = html;
+            document.getElementById(containerId).appendChild(template.content);
+        })
+        .then(c => {
+            const cursor = document.getElementById('cursor');
+            const textContent = 'MAHER SALEH';
+
+            for (let i = 0; i < textContent.length; i++) {
+                let span = document.createElement('span');
+                span.classList.add('text');
+                span.style.setProperty('--i', i + 1);
+                span.style.left = i * 0.6 + 'em';
+                span.style.filter = `hue-rotate(${i * 10}deg)`;
+                span.textContent = textContent[i];
+                cursor.appendChild(span);
+            }
         })
         .catch(err => console.error(err));
 }
